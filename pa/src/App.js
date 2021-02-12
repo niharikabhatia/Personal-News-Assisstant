69615f77644c176d2d68460759d5e336effca89e
Niharika <niharikabhatia62@gmail.com>
2021-02-12 21:55:09 +0530

    News Personal Assisstant

---
+++ pa/src/App.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import logo from './logo.png'
+//import NewsCards from './components/NewsCards/NewsCards';
+//import modal from './components/modal/modal';
+import  NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles.js';
+
+const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: 'f7da6e5927e67dcca216ca30944a63722e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'instructions') {
+          setIsOpen(true);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+          </div>
+        ) : null}
+        <img className={classes.image} src={logo} height="50px" alt="mylogo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+    
+      {!newsArticles.length ? (
+        <div className={classes.footer}>
+          <Typography variant="body1" component="h2">
+            Created by
+            <a className={classes.link} href="https://www.linkedin.com/in/niharika-b-989b9515b/"> Niharika Bhatia</a> -
+            
+          </Typography>
+          <img className={classes.image} src={logo} height="50px" alt="mylogo" />
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default App;
